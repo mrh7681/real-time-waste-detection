e742c032691b04a53fc469bdbc6bdadff6954fb7
Matthew Hartle <mrh7681@rit.edu>
2026-07-06 12:34:36 -0400

    Updated default port

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ def dashboard_data():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Flask app exposing yolov5 models")
-    parser.add_argument("--port", default=5000, type=int, help="port number")
+    parser.add_argument("--port", default=10000, type=int, help="port number")
     args = parser.parse_args()
     
     with app.app_context():
